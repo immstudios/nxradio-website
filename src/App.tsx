89f306900278782891ff2605bbf715d1c8deaa5c
Innders <49156310+Innders@users.noreply.github.com>
2024-05-22 22:23:04 +0100

    feat: particle background

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { useRef, useEffect, useState } from "react";
+import { useRef, useEffect, useState, useMemo } from "react";
 import IcecastMetadataPlayer, {
   IcyMetadata,
   OggMetadata,
 } from "icecast-metadata-player";
 
 import styled from "styled-components";
+import Background from "./Background/Background";
 
 const streamUrl = "https://ice.nbla.xyz/nxradio";
 
@@ -21,19 +22,54 @@ const Main = styled.main`
 `;
 
 const PlayButton = styled.button`
-  width: 100px;
-  height: 100px;
+  width: 60px;
+  height: 60px;
   border-radius: 50%;
-  background-color: #000;
-  color: #fff;
+  color: #000;
+  background-color: #ffffffa7;
   font-size: 2rem;
   display: flex;
   justify-content: center;
   align-items: center;
   border: none;
   cursor: pointer;
+  padding: 0;
   &::before {
     content: "▶️";
+  }
+
+  &.isPlaying {
+    &::before {
+      content: "⏸";
+    }
+  }
+
+  &:focus {
+    outline: none;
+  }
+
+  transition: background-color 0.2s;
+  &:hover {
+    background-color: #ffffff;
+  }
+`;
+
+const PlayerWidget = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  justify-content: center;
+  align-items: center;
+
+  padding: 1rem 3rem;
+  border-radius: 2rem;
+  z-index: 10;
+  background-color: rgb(246 142 253 / 25%);
+  box-shadow: 0 0 10px 0 rgba(86, 86, 86, 0.2);
+
+  h2 {
+    margin: 0;
+    white-space: nowrap;
   }
 `;
 
@@ -63,20 +99,31 @@ function App() {
     };
   }, []);
 
+  const [isPlaying, setIsPlaying] = useState(false);
+
   const handlePlay = () => {
     if (playerRef.current) {
       if (playerRef.current.state === "playing") {
         playerRef.current.stop();
+        setIsPlaying(false);
       } else {
         playerRef.current.play();
+        setIsPlaying(true);
       }
     }
   };
+  const background = useMemo(() => <Background />, []);
 
   return (
     <Main>
-      <h1>{title}</h1>
-      <PlayButton onClick={handlePlay} />
+      {background}
+      <PlayerWidget>
+        <h2>{title || "Click to start playing"}</h2>
+        <PlayButton
+          onClick={handlePlay}
+          className={isPlaying ? "isPlaying" : "isPaused"}
+        />
+      </PlayerWidget>
     </Main>
   );
 }
---
+++ src/Background/Background.tsx
@@ -0,0 +1,10 @@
+import { FC } from "react";
+import ParticlesScene from "./Particles/Particles";
+
+interface BackgroundProps {}
+
+const Background: FC<BackgroundProps> = ({}) => {
+  return <ParticlesScene />;
+};
+
+export default Background;
